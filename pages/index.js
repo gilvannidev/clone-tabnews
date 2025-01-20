c68f8a62c0a3f3c11a8bdefb1a12e62fb657f59a
gilvannidev <160560369+gilvannidev@users.noreply.github.com>
2025-01-20 23:52:41 +0000

    modificar a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Papai eu te amo muito</h1>
+    return<h1>
+          Papai Gilvan eu te amo demais.Estou sempre com o senhor.
+          assinado:Vinicius teu filho
+          </h1>
 }
 
 export default Home;
