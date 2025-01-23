2a6d371691b89f71e617d07574235d501b2653e2
gilvannidev <160560369+gilvannidev@users.noreply.github.com>
2025-01-23 01:07:46 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return<h1>
-          Papai Gilvan eu te amo demais.Estou sempre com o senhor.
-          assinado:Vinicius teu filho
-          </h1>
+  return (
+    <h1>
+      Papai Gilvan eu te amo demais.Estou sempre com o senhor. assinado:Vinicius
+      teu filho
+    </h1>
+  );
 }
 
 export default Home;
